e65071d3bff51fc3ca6b37c40dc5dc786ff5624b
2k16daniel <2k16daniel@gmail>
2022-10-23 23:49:04 +0800

    initial project commit

---
+++ src/main/java/moomoo/springframework/moomoopetclinic/MoomooPetClinicApplication.java
@@ -0,0 +1,13 @@
+package moomoo.springframework.moomoopetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoomooPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoomooPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moomoo/springframework/moomoopetclinic/MoomooPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package moomoo.springframework.moomoopetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoomooPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
